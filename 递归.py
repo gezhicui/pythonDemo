d7d53245f66d393455de27a819cfbea8725feba5
yang <1372201820@qq.com>
2020-04-19 14:54:15 +0800

    函数变量进阶

---
+++ 递归.py
@@ -0,0 +1,12 @@
+def sum_numbers(num):
+    if num == 1:
+        return 1
+
+    # 假设 sum_numbers 能够完成 num - 1 的累加
+    temp = sum_numbers(num - 1)
+
+    # 函数内部的核心算法就是 两个数字的相加
+    return num + temp
+
+
+print(sum_numbers(3))
